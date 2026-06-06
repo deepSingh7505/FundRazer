f1b1ad92ccc7c3e11c87dc22fed57a67d5f358d6
DeepSingh7505 <deepsingh75053@gmail.com>
2026-06-06 19:49:03 +0530

    email all done

--- componets/Footer.js
+++ componets/Footer.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-900 text-white py-4 border-t border-white/10">
+    <footer className="bg-slate-900 text-white py-4 border-t border-white/10 flex ">
       <div className="max-w-6xl mx-auto px-4 text-center text-sm text-slate-300">
         © {new Date().getFullYear()} FundRazer || Built by Deep Singh
       </div>
-      <div>
+      <div className="max-w-6xl mx-auto px-4 text-center text-sm text-slate-300">
         Contect : deepsingh75053@gmail.com
       </div>
     </footer>
